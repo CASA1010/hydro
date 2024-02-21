44e2bc298d90d134a1552623d4f7d89d09e057da
unknown <claudiasilva10@yahoo.es>
2024-02-21 19:08:48 +0100

    fixed error on admin page

--- seed/delete_user.py
+++ seed/delete_user.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.append('C:\\Users\\Claudia\\Documents\\Informatik-Studium\\temp\\venv')
+sys.path.append('C:\\Users\\Claudia\\Documents\\Informatik-Studium\\temp\\venv\\backend')
 
-from backend.config import app
-from backend.models import User
-from backend.config import db
+from config import app
+from models import User
+from config import db
 
 app.app_context().push()
 
-user = User.query.get(3)
+user = User.query.get(7)
 db.session.delete(user)
 db.session.commit()
